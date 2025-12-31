de68e11a452441a31f0bf44283fb693ac121cdc6
promama <mamatofik771@gmail.com>
2025-12-31 18:39:22 +0100

    Create src/App.jsx - Main React component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Premium Card Showcase</h1>
+      <p>Welcome to cardsme</p>
+    </div>
+  )
+}
+
+export default App
